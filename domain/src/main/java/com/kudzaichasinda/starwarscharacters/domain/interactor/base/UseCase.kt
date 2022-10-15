908e973b5092278770f8d70d6711426bab595ad4
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2022-10-15 16:58:05 +0200

    delete unused files

--- domain/src/main/java/com/kudzaichasinda/starwarscharacters/domain/interactor/base/UseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.kudzaichasinda.starwarscharacters.domain.interactor.base
-
-/**
- * Base class for generic use case
- * */
-abstract class UseCase<T, in Params> {
-    abstract suspend operator fun invoke(params: Params): T
-}
